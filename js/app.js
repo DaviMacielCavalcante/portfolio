7f18207c92f798e8f2beb0296ef991fdd5b42b9b
Davi Cavalcante <93160711+DaviMacielCavalcante@users.noreply.github.com>
2023-05-16 11:10:06 -0300

    mais animações de texto

--- js/app.js
+++ js/app.js
@@ -1,7 +1,25 @@
+const sistemaClinica = document.getElementById('sistemaClinica');
+const apiFilmes = document.getElementById('apiFilmes');
+const pontosInteresse = document.getElementById('pontosDeInteresse');
+const jogo = document.getElementById('jogo');
+
 document.addEventListener('DOMContentLoaded', () => {
     new TypeIt("#textoApresentacao", {
         speed: 90,
         strings: "me chamo Davi, um quase poeta, apaixonado por inteligência artificial.",
         cursor: false
     }).go();    
+
+    sistemaClinica.addEventListener('mouseover', () => {
+        sistemaClinica.firstElementChild.firstElementChild.innerText = `Essencialmente: um CRUD`
+    });
+    
+    sistemaClinica.addEventListener('mouseout', () => {
+        sistemaClinica.innerHTML = `<a href="https://github.com/DaviMacielCavalcante/System_Hospital" target="_blank" id="sistemaClinica">
+        <h3>Sistema para Clínicas</h3>
+    </a>`
+    });
 });
+
+
+
