fd93dbd7764bcecde00c6fbf119cbe488f70321a
Davi Cavalcante <93160711+DaviMacielCavalcante@users.noreply.github.com>
2023-05-28 16:35:56 -0300

    Exclusão e Edição de Projetos finalizadas

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,7 @@ const apiFilmes = document.getElementById('apiFilmes');
 const pontosInteresse = document.getElementById('pontosDeInteresse');
 const jogo = document.getElementById('jogo');
 const elementos = document.querySelectorAll('.elementos');
+// Conhecimentos
 const elementosConhecimento = document.querySelectorAll('.elementosConhecimento');
 const java = document.getElementById('java');
 const js = document.getElementById('js');
@@ -10,10 +11,46 @@ const html = document.getElementById('html');
 const css = document.getElementById('css');
 const spring = document.getElementById('spring');
 const python = document.getElementById('python');
+
+// Nível de Editor
 const novoProjeto = document.getElementById('novoProjeto');
 const adicionarProjeto = document.getElementById('adicionarProjeto');
 const projeto = document.querySelector('#projeto');
 const listaProjetos = document.getElementById('listaProjetos');
+
+// Botões
+const deleteIcons = document.querySelectorAll('.fa-sharp.fa-solid.fa-circle-xmark.deleteEdit');
+const editIcons = document.querySelectorAll('.fa-sharp.fa-regular.fa-pen-to-square.deleteEdit');
+
+deleteIcons.forEach(icon => {
+    icon.addEventListener('click', (e) => {
+        const parentElement = e.target.parentElement.parentElement;
+        parentElement.remove();
+    });    
+});
+
+editIcons.forEach(icon => {
+    icon.addEventListener('click', (e) => {
+        const div = e.target.parentElement;
+        const link = div.querySelector('a');
+        const h3 = link.querySelector('h3');
+        const input = document.createElement('input');
+
+        input.type = 'text';
+        input.value = h3.textContent;
+
+        link.replaceChild(input, h3);
+
+        input.addEventListener('keydown', (e) => {
+            if (e.key === 'Enter') {
+                const novoH3 = document.createElement('h3');
+                novoH3.textContent = input.value;
+                link.replaceChild(novoH3, input);
+            }
+        });
+
+    })
+});
 
 novoProjeto.addEventListener('click', () => {
     adicionarProjeto.style.display = '';
@@ -60,75 +97,6 @@ window.addEventListener('DOMContentLoaded', () => {
     } else {
         document.getElementById('adicionarProjeto').style.display = 'none';
     }
-});
-
-elementos.forEach(elemento => elemento.addEventListener('mouseover', () => {
-    elemento.style.backgroundColor = 'rgba(41, 99, 138, 0)'    
-    elemento.style.border = '1px solid #2E9CCA'
-    elemento.firstElementChild.style.color = '#2E9CCA' 
-    elemento.firstElementChild.firstElementChild.style.color = '#2E9CCA'
-}));
-
-elementos.forEach(elemento => elemento.addEventListener('mouseout', () => {
-    elemento.style.backgroundColor = 'rgba(41, 100, 138)'    
-    elemento.style.border = '' 
-    elemento.firstElementChild.style.color = 'white' 
-    elemento.firstElementChild.firstElementChild.style.color = 'white'    
-}));
-
-elementosConhecimento.forEach(elemento => elemento.addEventListener('click', () => {
-    
-    elemento.style.backgroundColor = 'rgba(41, 100, 138)'    
-    elemento.style.border = '' 
-    elemento.firstElementChild.style.color = '#2E9CCA' 
-    elemento.firstElementChild.firstElementChild.style.color = '#2E9CCA'
-}));
-
-elementosConhecimento.forEach(elemento => elemento.addEventListener('click', () => {
-    elemento.style.backgroundColor = 'rgba(41, 99, 138, 0)'    
-    elemento.style.border = '1px solid #2E9CCA'
-    elemento.firstElementChild.style.color = 'white' 
-    elemento.firstElementChild.firstElementChild.style.color = 'white'    
-}));
-
-sistemaClinica.addEventListener('mouseover', () => {
-    sistemaClinica.firstElementChild.firstElementChild.innerText = `Um CRUD`
-});
-
-sistemaClinica.addEventListener('mouseout', () => {    
-    sistemaClinica.innerHTML = `<a href="https://github.com/DaviMacielCavalcante/System_Hospital" target="_blank" id="sistemaClinica">
-    <h3>Sistema de Clínicas</h3>
-</a>`;
-});
-
-apiFilmes.addEventListener('mouseover', () => {
-    apiFilmes.firstElementChild.firstElementChild.innerText = `Outro CRUD`
-});
-
-apiFilmes.addEventListener('mouseout', () => {
-    apiFilmes.innerHTML = `<a href="https://github.com/DaviMacielCavalcante/API-de-Filmes" target="_blank" id="sistemaClinica">
-    <h3>API de Filmes</h3>
-</a>`
-});
-
-pontosInteresse.addEventListener('mouseover', () => {
-    pontosInteresse.firstElementChild.firstElementChild.innerText = `Sistema de Localização`
-});
-
-pontosInteresse.addEventListener('mouseout', () => {
-    pontosInteresse.innerHTML = `<a href="https://github.com/DaviMacielCavalcante/Pontos-de-interesse" target="_blank" id="pontosDeInteresse">
-    <h3>Pontos de Interesse</h3>
-</a>`
-});
-
-jogo.addEventListener('mouseover', () => {
-    jogo.firstElementChild.firstElementChild.innerText = `Um dungeon-crawler`
-});
-
-jogo.addEventListener('mouseout', () => {
-    jogo.innerHTML = `<a href="https://github.com/DaviMacielCavalcante/dungeon-crawler" target="_blank" id="jogo">
-    <h3>Um jogo</h3>
-</a>`
 });
 
 java.addEventListener('click', () => {
